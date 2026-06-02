5dc816ee6a3b7ff8b1dd68774ab46339fcffeb39
root <root@runnervm3jyl0.uhqu1hbrjeguzohz4dsdardlhc.phxx.internal.cloudapp.net>
2026-06-02 19:51:06 +0000

    Replace Copy text label with Material Symbols content_copy icon

--- java/src/com/android/inputmethod/keyboard/internal/KeyboardIconsSet.java
+++ java/src/com/android/inputmethod/keyboard/internal/KeyboardIconsSet.java
@@ -58,6 +58,7 @@ public final class KeyboardIconsSet {
     public static final String NAME_ZWJ_KEY = "zwj_key";
     public static final String NAME_EMOJI_ACTION_KEY = "emoji_action_key";
     public static final String NAME_EMOJI_NORMAL_KEY = "emoji_normal_key";
+    public static final String NAME_COPY_KEY = "copy_key";
     public static final String NAME_PASTE_KEY = "paste_key";
 
     private static final SparseIntArray ATTR_ID_TO_ICON_ID = new SparseIntArray();
@@ -88,6 +89,7 @@ public final class KeyboardIconsSet {
         NAME_ZWJ_KEY,                     R.styleable.Keyboard_iconZwjKey,
         NAME_EMOJI_ACTION_KEY,            R.styleable.Keyboard_iconEmojiActionKey,
         NAME_EMOJI_NORMAL_KEY,            R.styleable.Keyboard_iconEmojiNormalKey,
+        NAME_COPY_KEY,                    R.styleable.Keyboard_iconCopyKey,
         NAME_PASTE_KEY,                   R.styleable.Keyboard_iconPasteKey,
     };
 
