17f5d58d4f3b8a665c1bb9fcb637736a986e7b99
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-06-05 20:08:55 -0300

    Remove logic around jni_latinimegoogle

--- java/src/com/android/inputmethod/latin/utils/JniUtils.java
+++ java/src/com/android/inputmethod/latin/utils/JniUtils.java
@@ -16,26 +16,14 @@
 
 package com.android.inputmethod.latin.utils;
 
-import android.util.Log;
-
 import com.android.inputmethod.latin.define.JniLibName;
 
 public final class JniUtils {
-    private static final String TAG = JniUtils.class.getSimpleName();
+    // The gesture lib (jni_latinimegoogle) is not available in this build.
+    public static final boolean sHaveGestureLib = false;
 
-    public static boolean sHaveGestureLib = false;
     static {
-        try {
-            System.loadLibrary(JniLibName.JNI_LIB_NAME2);
-            sHaveGestureLib = true;
-        } catch (UnsatisfiedLinkError ue) {
-            Log.e(TAG, "Could not load native library " + JniLibName.JNI_LIB_NAME2, ue);
-            try {
-                System.loadLibrary(JniLibName.JNI_LIB_NAME);
-            } catch (UnsatisfiedLinkError ule) {
-                Log.e(TAG, "Could not load native library " + JniLibName.JNI_LIB_NAME, ule);
-            }
-        }
+        System.loadLibrary(JniLibName.JNI_LIB_NAME);
     }
 
     private JniUtils() {
